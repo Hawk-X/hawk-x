9fcb6363c5aeb2b811e2bdeaf420b3f2a8070671
BORN2LOSE <born2lose@sigaint.org>
2017-04-26 17:21:30 +0300

    edit admin and add features

--- src/prox/admin.py
+++ src/prox/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+# Local Model.py
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+	"""Выводит название по first name, email and phone.
+	Фильтрует по дате создания.
+	Поиск по first, last name's and email."""
+	list_display = ['first_name', 'email', 'phone', 'created']
+	list_filter = ['phone','created']
+	search_fields = ['first_name', 'last_name', 'email']
+	class Meta:
+		model = Contact
+
+admin.site.register(Contact, ContactAdmin)		
