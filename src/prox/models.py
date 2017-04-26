847728e620d7df1a3225972ac24e16e5c950ff62
BORN2LOSE <born2lose@sigaint.org>
2017-04-26 17:21:56 +0300

    create model

--- src/prox/models.py
+++ src/prox/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+	"""Стандартная модель для обратной связи"""
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=30)
+	email = models.EmailField()
+	phone = models.CharField(max_length=25)
+	question = models.TextField(max_length=200)
+	created = models.DateField(null=True)
+	class Meta:
+		"""Сортировка по дате созданию. Свежие записи вверху.
+		За кастомные названия отвечает verbose_name."""
+		ordering = ['created']
+		verbose_name='cписок'
+		verbose_name_plural='Обратная связь'
+
+	def __str__(self):
+		return self.question
