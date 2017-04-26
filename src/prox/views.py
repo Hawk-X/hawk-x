aea49247212696fa797ac7de1262e45829cfe259
BORN2LOSE <born2lose@sigaint.org>
2017-04-26 17:22:13 +0300

    upd views

--- src/prox/views.py
+++ src/prox/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Contact
+from .forms import ContactForm
+
+
+def home(request):
+	contact_list = Contact.objects.all()
+	context = {'contact_list': contact_list}
+	return render(request, 'home.html', context)
+
+
+def contact(request):
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        # Если форма заполнена корректно, сохраняем все введённые пользователем значения
+        if form.is_valid():
+            # Rедирект на другую страницу
+            return render('/thanks/')
+    else:
+    	# Заполняем форму
+        form = ContactForm()
+        # Переходим на опр. странцу
+    return render(request, 'contact.html', {'form': form})
