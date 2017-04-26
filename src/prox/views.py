9ba59077c549a5a9c6ce450ce49167db03e90d61
BORN2LOSE <born2lose@sigaint.org>
2017-04-27 00:45:42 +0300

    # add js/min.js files

--- src/prox/views.py
+++ src/prox/views.py
@@ -17,8 +17,10 @@ def contact(request):
         form = ContactForm(request.POST)
         # Если форма заполнена корректно, сохраняем все введённые пользователем значения
         if form.is_valid():
+            # save чтобы сохранилось
+            form.save()
             # Rедирект на другую страницу
-            return render('/thanks/')
+            return render(request, 'thanks.html')
     else:
     	# Заполняем форму
         form = ContactForm()
