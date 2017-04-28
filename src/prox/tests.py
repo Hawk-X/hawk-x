1c8277ba288f280bf45f7150f83aec8710f5b6fa
BORN2LOSE <born2lose@sigaint.org>
2017-04-28 17:59:08 +0300

    # create test3

--- src/prox/tests.py
+++ src/prox/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+from .models import Contact
+
+
+class PageTest(TestCase):
+	"""Простой тест вьюхи"""
+	def home_page(self):
+		c = Client()
+		response = c.get("/")
+		self.assertEquals(response.status_code, 200)
+
+
+class ContactTest(TestCase):
+	def setUp(self):
+		Contact.objects.create(first_name="Dennis", question="UNIX is basically a simple operating system")
+		Contact.objects.create(last_name="Ritchie", question="but you have to be a genius to understand the simplicity")
+
+	def test_speak(self):
+		Dennis = Contact.objects.get(first_name="Dennis")
+		Ritchie = Contact.objects.get(last_name="Ritchie")
+		# self.assertEquals(Dennis.(), 'The Dennis says "UNIX is basically a simple operating system"')
+		# self.assertEquals(Dennis.speak(), 'The Ritchie says "but you have to be a genius to understand the simplicity"')
