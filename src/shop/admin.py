2d157ba3826f822acc6d4705a1a4da2876c18f2a
BORN2LOSE <born2lose@sigaint.org>
2017-04-30 22:02:17 +0300

    # create new class

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# Local Model.py
+from .models import TeslaCar
+
+
+class TeslaAdmin(admin.ModelAdmin):
+    """Вывод список по описанию, цене и мощности.Фильтр по типу машины.Поиск по описанию, типу и цене."""
+    list_display = ['title', 'price', 'engine_power']
+    list_filter = ['car_type']
+    search_fields = ['title', 'car_type', 'price']
+    class Meta:
+        model = TeslaCar
+
+admin.site.register(TeslaCar, TeslaAdmin)
