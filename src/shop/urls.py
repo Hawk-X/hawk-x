9b38adac8b0c03e161173e23f15a4b3722041c84
BORN2LOSE <born2lose@sigaint.org>
2017-04-30 22:07:13 +0300

    # CBV

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 # Local views shop
 from . import views
-
+# from .views import CarCreateView
 
 urlpatterns = [
-	url(r'^shop/', views.index, name='index'),
+    url(r'^shop/$', views.index, name='index'),
+    url(r'^shop/create/$', views.car_create, name='car_create'),
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+# from django.views import View
+
+from .models import TeslaCar
+from .forms import TeslaForm
 
 
 def index(request):
-	template_name = 'index.html'
-	return render_to_response(template_name)
+    items = TeslaCar.objects.all()
+    context = {'items': items}
+    template_name = 'index.html'
+    return render(request, template_name, context)
+
+
+# class CarCreateView(View):
+#     form_tesla = TeslaForm()
+#     template = 'partial_car_create.html'
+# 	html_form = render_to_string(template, context, request=request)
+#
+#     def get(self, request):
+#         form = self.form_tesla()
+#         return render(request, self.template, {'form': form})
+#
+#     def post(self, request):
+# 		form = self.form_tesla(request.POST)
+# 		if form.is_valid():
+# 			pass
+# 		form.save()
+# 		return redirect('/')
+#         return JsonResponse()
+
+def car_create(request):
+    form = TeslaForm()
+    context = {'form': form}
+    html_form = render_to_string('book/partial_car_create.html', context, request=request)
+    return JsonResponse({'html_form': html_form})
