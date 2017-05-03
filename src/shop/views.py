49d8d05587ea5123b6cadaf4eb59efc4e4e28b24
born2lose <born2lose@sigaint.com>
2017-05-03 06:48:27 +0300

    testing ajax create

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,21 +1,22 @@
+import six
 from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import render
 from django.views.generic.list import ListView
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 from .models import TeslaCar
-# from .forms import TeslaForm
+from .forms import TeslaForm
 
 
 class IndexView(ListView):
     model = TeslaCar
-    template_name = 'index.html'
+    template_name = 'shop/index.html'
 
     def get_queryset(self):
         if self.queryset is not None:
             queryset = self.queryset
-            if isinstance(queryset, QuerySet):
+            if isinstance(queryset):
                 queryset = queryset.all()
         elif self.model is not None:
             queryset = self.model._default_manager.all()
@@ -34,8 +35,20 @@ class IndexView(ListView):
             queryset = queryset.order_by(*ordering)
         return queryset
 
-# def car_create(request):
-#     form = TeslaForm()
-#     context = {'form': form}
-#     html_form = render_to_string('partial_car_create.html', context, request=request)
-#     return JsonResponse({'html_form': html_form})
+
+def car_create(request):
+    data = dict()
+
+    if request.method == 'POST':
+        form = TeslaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+        else:
+            data['form_is_valid'] = False
+    else:
+        form = TeslaForm()
+
+    context = {'form': form}
+    data['html_form'] = render('shop/partical_car_create.html', context, request=request)
+    return JsonResponse(data)
